77637f6271edaccef2bb1d71975c46d5feca07ac
kwebster2 <kevin.r.webster@gmail.com>
2016-06-06 16:14:01 -0400

    added basic navbar w/o search functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :require_login
 
+  helper_method def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
   private
   def require_login
     if !session[:user_id]
