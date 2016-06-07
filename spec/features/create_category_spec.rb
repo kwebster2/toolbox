dd5c8207c1967f7a1ed0ed34d6f5893f1e4fe0c7
kwebster2 <kevin.r.webster@gmail.com>
2016-06-07 18:21:13 -0400

    added login and create resource tests

--- spec/features/create_category_spec.rb
+++ spec/features/create_category_spec.rb
@@ -1,16 +1,19 @@
-feature 'Capybara: Create Resource', :js => true do
+feature 'Capybara: Resources', :js => true do
   before :each do
     visit '/'
-    click_link 'Sign In'
+    click_link('Sign In')
+    click_link('Web')
   end
 
   scenario 'can create new resource' do
-    visit '/cohorts/web-development'
     click_button('+Post Resource')
-    fill_in('resource_name', :with => 'Google')
-    fill_in('resource_url', :with => 'https://www.google.com')
-    select('Rails', :from => 'resource_category_id')
-    click_button('Save Resource')
-    expect(page).to have_content("Google")
+    within("div.modal-content") do
+      fill_in('resource_name', :with => 'Google')
+      fill_in('resource_url', :with => 'https://www.google.com')
+      select('Rails', :from => 'resource_category_id')
+      click_button('Save Resource')
+    end
+    wait_for_ajax
+    expect(Resource.last.name).to eq("Google")
   end
 end
--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Bookmark, type: :model do
 
+  let(:bookmark) {Bookmark.first}
+  let(:user) {User.first}
+  let(:resource) {Resource.first}
+
+  it 'belongs to a user' do
+    expect(bookmark.user).to eq(user)
+  end
+
+  it 'belongs to a resource' do
+    expect(bookmark.resource).to eq(resource)
+  end
+
 end
