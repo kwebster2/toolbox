4311880687715d36409ac8910aebfd669d2f21ec
kwebster2 <kevin.r.webster@gmail.com>
2016-06-07 19:04:16 -0400

    home controller actions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   skip_before_action :require_login, only: [:index]
 
   def index
-    redirect_to cohorts_path if current_user   
+    redirect_to cohorts_path if current_user
   end
 end
